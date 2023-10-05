944db8ff725bd5fb0fd4db9f54e2e1f30723d6d1
KATE_FISCHER <KATME2006@GMAIL.COM>
2023-10-04 19:23:06 -0500

    Basic Linear Searches - All Tests Passed

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,28 @@
+//linearSearch("a", bananaList); // 1
+
+bananaList = "bananas".split(""); // ["b", "a", "n", "a", "n", "a", "s"]
+
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  if (arr.indexOf(searchTerm) !== -1) {
+    return arr.indexOf(searchTerm);
+  } else {
+    return undefined;
+  }
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  indexArr = [];
+
+  for(i=0;i<arr.length;i++){
+    if(arr[i] === searchTerm){
+      indexArr.push(i)
+    }
+  }
+
+  return indexArr;
 }
+
+//console.log(linearSearch('n',bananaList))
+console.log(globalLinearSearch("n", bananaList));
 
 module.exports = { linearSearch, globalLinearSearch };
